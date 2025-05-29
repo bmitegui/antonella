3597feb71f1e40aa4b2263cb610abe27f5579c85
Brian Mite <bmite@hubilogist.com>
2025-05-29 14:15:50 -0500

    Widget de remember me

---
+++ lib/core/widgets/circle_check_box.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CircleCheckbox extends StatefulWidget {
+  final bool checked;
+  final Function(bool) onTap;
+  const CircleCheckbox({super.key, required this.checked, required this.onTap});
+
+  @override
+  State<CircleCheckbox> createState() => _CircleCheckboxState();
+}
+
+class _CircleCheckboxState extends State<CircleCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => widget.onTap(!widget.checked),
+        child: Container(
+            width: 24,
+            height: 24,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+            child: widget.checked
+                ? Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 5),
+                        color: Color(0xFF999999)))
+                : null));
+  }
+}
--- lib/features/user/presentation/widgets/remember_me_widget.dart
+++ lib/features/user/presentation/widgets/remember_me_widget.dart
@@ -1,5 +1,5 @@
+import 'package:antonella/core/widgets/circle_check_box.dart';
 import 'package:flutter/material.dart';
-import 'package:antonella/core/l10n/app_localizations.dart';
 
 class RememberMeWidget extends StatelessWidget {
   final bool value;
@@ -9,13 +9,13 @@ class RememberMeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final texts = AppLocalizations.of(context)!;
     final textTheme = Theme.of(context).textTheme;
     return Row(children: [
-      Text('Recuérdame', style: textTheme.bodySmall!
-                    .copyWith(color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold)),
-      //const Spacer(),
-      //Switch(value: value, onChanged: onChanged)
+      CircleCheckbox(checked: value, onTap: onChanged),
+      const SizedBox(width: 8),
+      Text('Recuérdame',
+          style: textTheme.bodySmall!.copyWith(
+              color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold))
     ]);
   }
 }
