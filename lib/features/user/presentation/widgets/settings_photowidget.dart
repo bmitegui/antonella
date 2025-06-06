8d6a14679686ab5e9a24f3c01c9644dc1f22dc2b
Ghost04102002 <edusemac@espol.edu.ec>
2025-06-05 19:01:57 -0500

    Ajustes a  ajustes

---
+++ lib/features/user/presentation/widgets/settings_photowidget.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:antonella/core/theme/responsive_size.dart';
+import 'package:antonella/features/user/presentation/widgets/upload_user_photo_widget.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPhotowidget extends StatelessWidget {
+  const SettingsPhotowidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, border: Border.all()),
+          child: CircleAvatar(
+              radius: 80.rh(context),
+              backgroundColor: Colors.white,
+              backgroundImage:
+                  const AssetImage('assets/img/placeholder_women.png'))),
+      /*Positioned(
+          bottom: 0,
+          right: 0,
+          child: GestureDetector(
+              onTap: () async => await showModalBottomSheet<List>(
+                  backgroundColor: Colors.white,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const UploadUserPhotoWidget();
+                  }),
+              child: Container(
+                  decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        offset: const Offset(0, 4),
+                        blurRadius: 6)
+                  ]),
+                  child: CircleAvatar(
+                      radius: 24.rh(context),
+                      backgroundColor: const Color(0XFFBD818E),
+                      child: const Icon(Icons.add_a_photo_outlined,
+                          color: Colors.white)))))*/
+    ]);
+  }
+}
